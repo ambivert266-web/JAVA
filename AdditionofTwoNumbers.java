a6c785b7eb4c0fa14bda25dd7a82a1e000044308
VARMA Srishti <ambivert266@gmail.com>
2026-02-16 14:18:36 +0000

    Fix addition operation in AdditionofTwoNumbers.java

--- AdditionofTwoNumbers.java
+++ AdditionofTwoNumbers.java
@@ -10,11 +10,12 @@ int num1=Sc.nextInt();
 System.out.println("enter second number");
 int num2=Sc.nextInt();
 
-int sum=num1*num2;
+int sum=num1+num2;
 
 System.out.println("Addition of Two Numbers"+sum);
 }
 }
+
 
 
 
